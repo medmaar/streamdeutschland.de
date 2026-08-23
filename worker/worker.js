621af072e672e4ca4f44bea84af17141f33691f2
Mo <mo@iptv.com>
2026-08-23 22:12:33 +0000

    Update Trex server URL to mag.trexlive.me

--- worker/worker.js
+++ worker/worker.js
@@ -5,7 +5,7 @@
 
 const API_BASE    = "https://activationpanel.ru/api/api.php";
 const API_KEY     = "35cf68cc83a3a82e1a0ac5361c7b6105";
-const HOST        = "http://terry.thecontentnest.com";
+const HOST        = "http://mag.trexlive.me";
 const RESEND_KEY  = "re_JUbF4roD_GFX9RoBM9NHyRfSNiWbDc3U3";
 const FROM_EMAIL  = "StreamDeutschland <kontakt@streamdeutschland.de>";
 const ADMIN_EMAIL = "kontakt@streamdeutschland.de";
