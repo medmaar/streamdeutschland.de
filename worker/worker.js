abccc35387e9936cb79fb72c3ea2c01ef61b4d66
Mo <mo@iptv.com>
2026-09-03 22:04:35 +0000

    Update server URL to line.truthdaily.me

--- worker/worker.js
+++ worker/worker.js
@@ -5,7 +5,7 @@
 
 const API_BASE    = "https://activationpanel.ru/api/api.php";
 const API_KEY     = "35cf68cc83a3a82e1a0ac5361c7b6105";
-const HOST        = "http://mag.trexlive.me";
+const HOST        = "http://line.truthdaily.me";
 const FROM_EMAIL  = "StreamDeutschland <kontakt@streamdeutschland.de>";
 const ADMIN_EMAIL = "kontakt@streamdeutschland.de";
 const SITE_URL    = "https://streamdeutschland.de";
